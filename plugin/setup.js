a47b15410c768a952600e271daf52c9e5933d244
Benoit Pierre <benoit.pierre@gmail.com>
2015-02-10 18:23:17 +0100

    setup: drop "I don't care about cookies" addon
    Can be replaced by uBlock 3rd party filter list.
    (EU: Prebake - Filter Obtrusive Cookie Notices)

--- plugin/setup.js
+++ plugin/setup.js
@@ -385,12 +385,6 @@ commands.addUserCommand(
 
       // }}}
 
-      // I don't care about cookies. {{{
-
-      installAddonIfNotAlready("I don't care about cookies", 'jid1-KKzOGWgsW3Ao4Q@jetpack');
-
-      // }}}
-
       // Privacy Badger. {{{
 
       installAddonIfNotAlready('Privacy Badger', 'jid1-MnnxcxisBPnSXQ@jetpack');
